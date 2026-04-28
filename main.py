8fe760c79d6108da3dac35ad7766ff620c911441
Felipe <felipecezaralmeida0@gmail.com>
2026-04-28 08:49:31 -0300

    1. Organização e instalação de dependências

---
+++ main.py
@@ -0,0 +1,5 @@
+from fastapi import FastAPI, Depends, HTTPException, Request, Form
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.templating import Jinja2Templates
+from sqlalchemy.orm import Session
+
