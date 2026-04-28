a6138ba9b08d90422ff0726aaa7c26407ba73fd5
Felipe <felipecezaralmeida0@gmail.com>
2026-04-28 13:03:57 -0300

    7. html indo em frente

--- main.py
+++ main.py
@@ -5,3 +5,18 @@ from sqlalchemy.orm import Session
 from database import get_db
 from models import Categoria, Produto
 
+app = FastAPI(title="Vendo comida")
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/")
+def home(request: Request):
+    return templates.TemplateResponse(
+     request,
+    "comida.html",
+    {"request": request}
+    )
+
+@app.get("/produto/cadastro", response_class=HTMLResponse)
+def exibir_cadastro(request: Request):
+    return templates.TemplateResponse(request, "cadastro_produto.html", {"request": request})
